6d51a6df60fa2e6326a8e731e184fe642630cc12
gaojunbao <gaojunbao@buba.com>
2021-07-20 19:26:54 +0800

    第三次提交

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello{
-	public Static void main(String[] args){
-		System.out.println("nihao");
-	}
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("nihao3333333");
+    }
 }
